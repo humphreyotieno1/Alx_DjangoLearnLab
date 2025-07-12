7266058c2beb3dd53369f2f75647ff8908874ee3
humphreyotieno1 <humphreyotieno04@gmail.com>
2025-07-12 11:45:41 +0300

    feat:creating admin panel

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # Display these fields in the list view
+    list_display = ('title', 'author', 'publication_year')
+    
+    # Enable filtering by these fields
+    list_filter = ('publication_year', 'author')
+    
+    # Enable search in these fields
+    search_fields = ('title', 'author')
+    
+    # Order by title by default
+    ordering = ('title',)
