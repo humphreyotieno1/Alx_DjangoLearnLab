07dda33ebcf5b38bffa2fcf14d73bc93fd77dec7
humphreyotieno1 <humphreyotieno04@gmail.com>
2025-07-27 10:55:39 +0300

    refactor:updated user model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,11 +1,36 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
 import os
 
 def user_profile_photo_path(instance, filename):
     # File will be uploaded to MEDIA_ROOT/user_<id>/<filename>
     return f'user_{instance.id}/profile_photos/{filename}'
+
+class CustomUserManager(BaseUserManager):
+    """Custom user model manager where email is the unique identifier."""
+    
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular user with the given email and password."""
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
     """
@@ -26,9 +51,15 @@ class CustomUser(AbstractUser):
         help_text=_('Profile photo of the user')
     )
     
+    objects = CustomUserManager()
+    
     class Meta:
         verbose_name = _('user')
         verbose_name_plural = _('users')
+        
+    # Use email as the unique identifier instead of username
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
     
     def __str__(self):
         return self.username
