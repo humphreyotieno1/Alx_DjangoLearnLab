8718380863370c939f280ddf9ea0e38d6a47dd72
humphreyotieno1 <humphreyotieno04@gmail.com>
2025-08-09 12:33:33 +0300

    Views and authentication

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BookListCreateView, BookDetailView, AuthorListCreateView, AuthorDetailView, BookUpdateView, BookDeleteView, BookDestroyView
+from .views import BookListCreateView, BookDetailView, AuthorListCreateView, AuthorDetailView, BookUpdateView, BookDeleteView, BookDestroyView, BookCreateView
 
 urlpatterns = [
     path('books/', BookListCreateView.as_view()),
@@ -7,6 +7,8 @@ urlpatterns = [
     path('authors/', AuthorListCreateView.as_view()),
     path('authors/<int:pk>/', AuthorDetailView.as_view()),
     path('books/<int:pk>/update/', BookUpdateView.as_view()),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view()),
-    path('books/<int:pk>/destroy/', BookDestroyView.as_view()),
+    path('books/delete/', BookDeleteView.as_view()),
+    path('books/destroy/', BookDestroyView.as_view()),
+    path('books/create/', BookCreateView.as_view()),
+    path('books/update/', BookUpdateView.as_view()),
 ]
