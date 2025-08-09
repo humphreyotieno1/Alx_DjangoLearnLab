890360478ea38280334dd12ad67d1af95bb1654a
humphreyotieno1 <humphreyotieno04@gmail.com>
2025-08-09 13:07:42 +0300

    corrections

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -18,6 +18,8 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
     path('books/create/', views.BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete'),
+    path('books/update/', views.BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/', views.BookDeleteView.as_view(), name='book-delete'),
+    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update-pk'),
+    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete-pk'),
 ]
