6bb902bd7f80f0a45f91f9f388f29ca54debb997
humphreyotieno1 <humphreyotieno04@gmail.com>
2025-08-15 22:29:03 +0300

    correct:update routes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('posts/', views.PostListView.as_view(), name='post_list'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post_create'),
-    path('posts/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
